aef0fdbece8aba2fd1bc7964d5577cfa19aa1e85
ZYCFan <1543558411@qq.com>
2016-10-24 13:29:49 +0800

    幼儿园家长版

---
+++ MDKG_Patriarch/MDKinderGartenPTests/MDKinderGartenTests.m
@@ -0,0 +1,39 @@
+//
+//  MDKinderGartenTests.m
+//  MDKinderGartenTests
+//
+//  Created by zhouyongchao on 16/1/22.
+//  Copyright © 2016年 zhouyongchao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MDKinderGartenTests : XCTestCase
+
+@end
+
+@implementation MDKinderGartenTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
